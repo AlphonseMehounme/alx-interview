2cf6de3e32d98cd7e757923f7a141ecaed3f4e17
Alphonse <alphonsemehounme9@gmail.com>
2024-09-20 19:58:55 -0700

    update request

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,12 +3,22 @@ const movieId = process.argv[2];
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-request(url, (error, response, body) => {
-  const characters = JSON.parse(body).characters;
-  for (let i = 0; i < characters.length; i++) {
-    request(characters[i], (error, response, body) => {
-      const character = JSON.parse(body).name;
-      console.log(character);
-    });
-  }
-});
+async function charac () {
+  await request(url, async function req (error, response, body) {
+    if (error) {
+      console.error('There is an error');
+    }
+    const characters = await JSON.parse(body).characters;
+    for (let i = 0; i < characters.length; i++) {
+      await request(characters[i], async function name (error, response, body) {
+        if (error) {
+          console.error('Error');
+        }
+        const character = await JSON.parse(body).name;
+        await console.log(character);
+      });
+    }
+  });
+}
+
+charac();
