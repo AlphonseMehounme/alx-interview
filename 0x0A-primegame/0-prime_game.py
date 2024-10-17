aa485c27afc63e01c3022263f13d455afd837c5f
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-10-17 23:59:14 +0100

    thePrimes

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,6 +3,12 @@
 Prime Game
 """
 
+def thePrimes(n):
+    """
+    list of prime less than n
+    """
+    return [2]
+
 def isWinner(x, nums):
     """
     determine the winner at the end of rounds
